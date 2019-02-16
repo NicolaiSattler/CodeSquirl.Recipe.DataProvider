2fc8964e2d3b48dae41bab4d67d257fe89740b1f
Nicolai Sättler <n.sattler@dataquint.nl>
2019-02-16 12:11:05 +0100

    develop | added: new implementations for crud operations. todo --> logging

--- src/Repository/ProductRepository.cs
+++ src/Repository/ProductRepository.cs
@@ -2,8 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
 using CodeSquirl.System;
 using Dapper;
 
@@ -11,14 +9,45 @@ namespace CodeSquirl.RecipeApp.DataProvider
 {
     public class ProductRepository : IRepository<ProductDTO>
     {
-        private const string SELECT_ALL = "SELECT * FROM \"CodeSquirl\".\"Product\"";
-        private const string INSERT = "INSERT INTO \"CodeSquirl\".\"Product\"(\"UniqueID\", \"Name\", \"Type\", \"Perishable\", \"Deleted\") VALUES (@UniqueID, @Name, @Type, @Perishable, @Deleted)"; 
+        private const string TABLE_NAME = "\"CodeSquirl\".\"Product\"";
 
+        private const string INSERT = "INSERT INTO \"CodeSquirl\".\"Product\"" +
+                                      "(\"UniqueID\", \"Name\", \"Type\", \"Perishable\", \"Deleted\") " +
+                                      "VALUES (@UniqueID, @Name, @Type, @Perishable, @Deleted)";
+
+        private const string UPDATE = "UPDATE \"CodeSquirl\".\"Product\" " +
+                                      "SET \"Name\" = @Name, \"Type\" = @Type, \"Perishable\" = @Perishable " +
+                                      "WHERE \"UniqueID\" = @UniqueID";
+        
         private readonly IDbConnection _connection;
+        protected QueryBuilder Builder { get; }
 
         public ProductRepository(IDbConnection connection)
         {
             _connection = connection;
+
+            Builder = new QueryBuilder(TABLE_NAME);
+        }
+
+        private object[] AddRangeParameters(IEnumerable<ProductDTO> entities)
+        {
+            var index = 0;
+            var length = entities.Count();
+            var result = new object[length];
+
+            foreach(var item in entities)
+            {
+                result[index] = new 
+                {
+                    item.UniqueID,
+                    item.Name,
+                    Type = (int)item.Type,
+                    item.Perishable, 
+                    Deleted = false
+                };
+            }
+
+            return result;
         }
 
         public bool Add(ProductDTO entity)
@@ -30,9 +59,9 @@ namespace CodeSquirl.RecipeApp.DataProvider
                 var result = _connection.Execute(INSERT, entity);
                 return result > 0;
             }
-            catch (Exception)
+            catch (Exception ex)
             {
-                throw;
+                return false;
             }
             finally
             {
@@ -41,21 +70,36 @@ namespace CodeSquirl.RecipeApp.DataProvider
             }
         }
 
-        public void AddRange(IEnumerable<ProductDTO> entities)
+        public bool AddRange(IEnumerable<ProductDTO> entities)
         {
-            throw new NotImplementedException();
+            try
+            {
+                _connection.Open();
+
+                var parameters = AddRangeParameters(entities);
+                return _connection.Execute(INSERT, parameters) > 0;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+            finally
+            {
+                _connection.Close();
+                _connection.Dispose();
+            }
         }
 
-        public IList<ProductDTO> GetAll()
+        public IList<ProductDTO> Get()
         {
             try 
             {
                 _connection.Open();
-                return _connection.Query<ProductDTO>(SELECT_ALL).ToList();
+                return _connection.Query<ProductDTO>(Builder.SelectAll).ToList();
             }
-            catch(Exception)
+            catch(Exception ex)
             {
-                throw;
+                return null;
             }
             finally
             {
@@ -64,14 +108,58 @@ namespace CodeSquirl.RecipeApp.DataProvider
             }
         }
 
-        public bool Remove(ProductDTO entiy)
+        public ProductDTO GetByID(Guid uniqueID)
         {
-            throw new NotImplementedException();
+            try
+            {
+                _connection.Open();
+                return _connection.QueryFirstOrDefault<ProductDTO>(Builder.SelectByID, new { UniqueID = uniqueID });
+            }
+            catch(Exception ex)
+            {
+                return null;
+            }
+            finally
+            {
+                _connection.Close();
+                _connection.Dispose();
+            }
+        }
+
+        public bool Remove(Guid uniqueID)
+        {
+            try
+            {
+                _connection.Open();
+                return _connection.Execute(Builder.DeleteByID, new { UniqueID = uniqueID }) > 0;
+            }
+            catch(Exception ex)
+            {
+                return false;
+            }
+            finally
+            {
+                _connection.Close();
+                _connection.Dispose();
+            }
         }
 
         public bool Update(ProductDTO entity)
         {
-            throw new NotImplementedException();
+            try
+            {
+                _connection.Open();
+                return _connection.Execute(UPDATE, entity) > 0;
+            }
+            catch(Exception ex)
+            {
+                return false;
+            }
+            finally
+            {
+                _connection.Close();
+                _connection.Dispose(); 
+            }
         }
     }
 }
